5ebf3f4c71922caf187cd405bbe2996d720de459
Lex Miin <alexmiyin@gmail.com>
2025-06-14 15:05:42 +0300

    docs: readme

---
+++ README.md
@@ -0,0 +1,40 @@
+> [!WARNING]
+> This crate was written for learning purposes and contains an untested, unoptimized version of the DEFLATE algorithm.
+> For production use, consider well-tested libraries like `flate2`.
+
+# `encrust`
+
+A simple gzip decompression tool written in Rust, implementing the DEFLATE algorithm (decoding part) from scratch.
+
+## Why?
+
+The motivation is simply to learn new things. This is my first project written in Rust, and also the first time I delve into compression algorithms.
+
+I've decided to implement the DEFLATE algorithm because I wanted to learn more about bit arithmetic and how to use it in real-world programs.
+
+## What it does
+
+This tool can decompress gzip files by implementing:
+
+- Gzip header parsing (including optional fields like filename, comments, extra data)
+- DEFLATE decompression with support for:
+  - Uncompressed blocks
+  - Fixed Huffman coding
+  - Dynamic Huffman coding
+- Bit-level stream reading
+
+## Usage
+
+The tool will decompress the gzip file and print:
+
+- The decoded content (if it's valid UTF-8)
+- Parsed header information (filename, modification time, OS, etc.)
+
+## Implementation Notes
+
+The DEFLATE implementation was referenced from [madler/zlib](https://github.com/madler/zlib/blob/master/contrib/puff/puff.c).
+
+## Learning Resources
+
+- [RFC 1951 - DEFLATE Specification](https://tools.ietf.org/html/rfc1951)
+- [RFC 1952 - GZIP Specification](https://tools.ietf.org/html/rfc1952)
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,33 @@
 use std::fs;
 
 use anyhow::{Context, Result};
-use clap::{Parser, Subcommand};
-use kos::gzip::Decoder;
-use kos::huffman::Huffman;
-
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Extract,
-    Compress,
-}
+use encrust::gzip::Decoder;
 
 fn main() -> Result<()> {
-    let cli = Cli::parse();
+    let Some(filename) = std::env::args().nth(1) else {
+        return Err(anyhow::anyhow!("file must be provided"));
+    };
 
-    match &cli.command {
-        Some(Commands::Extract) => {
-            let input = fs::read("./test_uncompressed.gz").context("read test file")?;
-            let mut decoder = Decoder::new(&input);
-            match decoder.decode() {
-                Ok(output) => {
-                    if let Ok(decoded) = String::from_utf8(output) {
-                        println!("Got decoded contents:\n{decoded}");
-                    } else {
-                        println!("invalid utf-8 string");
-                    }
-                    println!(
-                        "Parsed Header:\nName: {}, ModTime: {}, OS: {}, Extra: {:?}, Comment: {}",
-                        decoder.header.name,
-                        decoder.header.modtime,
-                        decoder.header.os,
-                        decoder.header.extra,
-                        decoder.header.comment
-                    );
-                }
-                Err(e) => return Err(e),
-            };
+    let input = fs::read(filename).context("read provided file")?;
+    let mut decoder = Decoder::new(&input);
+    match decoder.decode() {
+        Ok(output) => {
+            if let Ok(decoded) = String::from_utf8(output) {
+                println!("Got decoded contents:\n{decoded}");
+            } else {
+                println!("invalid utf-8 string");
+            }
+            println!(
+                "Parsed Header:\nName: {}, ModTime: {}, OS: {}, Extra: {:?}, Comment: {}",
+                decoder.header.name,
+                decoder.header.modtime,
+                decoder.header.os,
+                decoder.header.extra,
+                decoder.header.comment
+            );
         }
-        Some(Commands::Compress) => {
-            let huffman = Huffman::from("A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED");
-            let encoded = huffman.encode();
-            println!("{encoded}");
-            println!("{}", huffman.decode(&encoded));
-        }
-        _ => {
-            println!("this can happen?");
-        }
-    }
+        Err(e) => return Err(e),
+    };
 
     Ok(())
 }
